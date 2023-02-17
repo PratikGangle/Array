b48ca9070d890eca752a220561e39b037c159540
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-02-17 17:30:27 +0530

    Time: 1 ms (100.00%), Space: 50.6 MB (50.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,34 +3,21 @@ class Solution {
         
         int size = nums.length;
         
-        int[] prefix = new int[size];
-        int [] suffix = new int[size];
+        int [] ans = new int[size];
         
-          int [] ans = new int[size];
-        
-        prefix[0] = 1;
-        suffix[size-1] = 1;
-        
-        
+        ans[0] = 1;
+               
         for(int i=1 ; i<nums.length;  i++){            
-            prefix[i] = nums[i-1] * prefix[i-1];
+            ans[i] = nums[i-1] * ans[i-1];
+        }
+                
+        int suffix =1;
+        for(int i=size-1; i>=0; i--){
+            ans[i]  *= suffix;
+            suffix *=  nums[i];             
         }
         
-        for(int i =size-2;  i>=0; i--  ){
-            suffix[i] = nums[i+1] * suffix[i+1];
-        }
-        
-        
-        for(int i=0; i<nums.length; i++){
-            ans[i] = prefix[i] * suffix[i];
-        }
-        
-        return ans;
-        
-        
-        
-        
-        
-        
+        return ans;                    
+              
     }
 }
