34afe0c3505483f9ba72814c39e583df28814842
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-02-18 22:16:54 +0530

    Time: 3 ms (93.11%), Space: 51.9 MB (98.87%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,32 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
         
-         int i = 0;
-        int j = height.length - 1;
-        int max = 0;
-        // max variable store the maximum amount of water
-        while (i <= j) {
-            int min = 0; 
-            //find the minimum height of the veritcal line
-
-            if (height[i] < height[j]) {
-                min = height[i];
-            } else {
-                min = height[j];
-            }
-            min = min * (j - i);
-
-            if (min > max) {
-                max = min;
-            }
-
-            if (height[i] < height[j]) {
-                i++;
-            } else if (height[j] <= height[i]) {
-                j--;
-            }
-        }        
-        return max;       
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
         
+        while(left<right){
+            
+           maxWater =Math.max(maxWater,(right-left)* Math.min(height[left],height[right]));
+            if(height[left]<height[right]) left++;
+            else right--;
+                
+        }         
+        return maxWater;                 
     }
 }
