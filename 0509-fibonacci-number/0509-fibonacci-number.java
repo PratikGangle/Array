bc18446ad1685bd66eef073ee4665049ed71e70d
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-03-04 22:02:45 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (22.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -4,8 +4,15 @@ class Solution {
         if(n==0 || n==1){
             return n;
         }
+        int first=0;
+        int second =1;
         
-        return fib(n-2) + fib(n-1);
-        
+        int ans=0;
+        for(int i=2; i<=n; i++){            
+            ans = first + second;
+            first = second;
+            second = ans;
+        }
+        return ans;       
     }
 }
