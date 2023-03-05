f2028959ebfcefe1556746c9c98bd2aa29de8042
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-03-05 11:36:52 +0530

    Time: 0 ms (100.00%), Space: 43 MB (74.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        int flag;
-        for(int i=0; i<= nums.length; i++){
-             flag =-1;            
-            for(int j=0; j<nums.length; j++){
-                
-                if(nums[j] ==i){
-                    flag =0;
-                    break;
-                }
-            }
-            if(flag ==-1){
-                return i;
-            }
-        } 
+        int arrSum=0;
+        for(int i=0; i< nums.length; i++){
+            arrSum += nums[i];
+        }
+        int n= nums.length;
+        int sum = (n*(n+1))/2;
         
-        return -1;
+        
+        return sum-arrSum;
     }
 }
