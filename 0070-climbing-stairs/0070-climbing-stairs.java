0bf244a793d3dc20bc15ac7d28c83893b1caf217
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-03-07 19:22:14 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (51.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,26 +7,17 @@ class Solution {
         // fill the array 
         Arrays.fill(arr,-1);
         
-        // use helper function to pass array
-            helper(n, arr);
-        
+        arr[0] =1;
+        //tabulation loop
+        for(int i=1; i<=n; i++){
+            if(i==1){
+                arr[i] = arr[i-1];
+            }
+            else{
+                arr[i] = arr[i-1] + arr[i-2];
+            }
+        }
        // return the answer
-        return arr[n];
-    }
-    
-    public int helper(int n, int arr[]){
-         if(n==0){
-            return 1;
-        }
-        if( n< 0){
-            return 0;
-        }
-        
-        if(arr[n] != -1){
-            return arr[n];
-        }
-        
-        arr[n] = helper(n-1,arr) + helper(n-2,arr);      
         return arr[n];
     }
 }
