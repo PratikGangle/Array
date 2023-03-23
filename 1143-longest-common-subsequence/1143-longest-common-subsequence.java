ccfede6fbe108224fbbd049da5c1c41b240ce2af
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-03-23 16:52:37 +0530

    Time: 29 ms (35.01%), Space: 50.3 MB (37.70%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,27 +6,17 @@ class Solution {
         
         int dp[][] = new int[n+1][m+1];
         
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m ; j++){
-                dp[i][j] = -1;
-            }
-        }
-       return LCS(text1, text2, n , m , dp);
-    }
-    
-    public int LCS(String s1 , String s2 , int  n , int m , int[][] dp){
-        
-        if(n ==0 || m == 0){
-            return 0;
-        }
-        if(dp[n][m] != -1){
-            return dp[n][m];
-        }
-        if(s1.charAt(n-1) == s2.charAt(m-1)){
-             return  dp[n][m] = 1 + LCS(s1,s2,n-1, m-1,dp);
-        }
-        else{
-           return  dp[n][m] = Math.max( LCS(s1,s2,n,m-1,dp)  ,LCS(s1,s2,n-1,m,dp));
-        }
+       for(int i=1; i<=n; i++){
+           for(int j=1; j<=m ; j++){
+
+               if(text1.charAt(i-1)  == text2.charAt(j-1)){
+                   dp[i][j] = 1 + dp[i-1][j-1];
+               }
+               else{
+                   dp[i][j] = Math.max( dp[i-1][j] , dp[i][j-1] );
+               }
+           }
+       }
+        return dp[n][m];
     }
 }
