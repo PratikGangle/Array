a6c13c75caf2ec48089cc07d7e626490f67f42b4
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-04-05 09:55:32 -0700

    Time: 1 ms (92.37%), Space: 42.1 MB (67.08%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,27 +1,50 @@
 class Solution {
     public int thirdMax(int[] nums) {
+        
+        if(nums.length == 1){
+            return nums[0];
+        }
+        
+        if(nums.length ==2){
+            return Math.max(nums[0], nums[1]);
+        }
+        
+        long m1 = Long.MIN_VALUE;
+        long m2 =Long.MIN_VALUE;
+        long m3 =Long.MIN_VALUE;
+        
+        // System.out.print(m3);
+            
+        
+        for(int i : nums){
+            
+            if(i == m1 || i== m2  || i == m3){
+                continue;
+            }
+            
+            if(i >= m1){
+                m3 = m2;
+                m2 =m1;
+                m1 =i;
+            }
+            
+            else if(i>=m2){
+                m3=m2;
+                m2 =i;
+            }
+            
+            else if( i>=m3){
+                m3 =i;
+            }
+            
+            
+        }
+        
+        if(m3 == Long.MIN_VALUE){
+            return  (int) m1;
+        }
+        
+        return  (int) m3;
        
-        TreeSet<Integer> set = new TreeSet<>();
-        
-        for(int i=0; i<nums.length; i++){
-            set.add(nums[i]);
-        }
-        
-        int [] arr = new int[set.size()];
-        
-        int j=0;
-       for(Integer i : set){
-           arr[j++]  =i;
-       }
-        
-      
-        
-        if(arr.length <=2){
-            return arr[arr.length-1];
-        }
-        
-        
-        return arr[arr.length -3];
-      
     }
 }
