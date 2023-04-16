4d9b08e91aa30b782e81508072737ef72d67a456
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-04-16 09:42:19 -0700

    Time: 4 ms (87.00%), Space: 53.5 MB (94.86%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,44 +9,47 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {  
+    public boolean isPalindrome(ListNode head) {
         
-        int size =0;        
-        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
         
-        // find the size of the linkedlist
-        while(temp !=null){
-            temp = temp.next;
-            size++;
-        }      
-        
-        int half = size/2;    
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        int i=1;           
-        // push half element in the stack;
-        temp = head;
-        while(i<=half)
-        {
-            stack.push(temp.val);
-            temp = temp.next;
-            i++;
+        // find the middle of the linkedlist 
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        // if linkedlist size is odd skip one element
-        if(size % 2 != 0){
-            temp = temp.next;            
-        }
-          
-        //check linkedlist is palindrome or not
-        while(temp != null){
-            int val  = temp.val;            
-            if(stack.pop() != val){
+        
+        //reverse the half side of the linkedlist
+        
+        slow.next = reverseList(slow.next);
+        
+        //move slow two right half
+        slow = slow.next;
+        
+        //check the left half and right half is equal or not
+        while(slow !=null){
+            if(head.val != slow.val){
                 return false;
             }
-            temp = temp.next;
+            head=head.next;
+            slow= slow.next;
         }
         
         return true;
+    }
+    
+    
+    public ListNode reverseList(ListNode head){
+        ListNode pre =null;
+        ListNode next =null;
+        
+        while(head!=null){
+            next = head.next;
+            head.next =pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
     }
 }
