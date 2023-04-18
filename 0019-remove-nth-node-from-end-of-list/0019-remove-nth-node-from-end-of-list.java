7fdb80fff05070a21cfa3242b48b1ec442eec528
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-04-18 09:51:10 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (20.24%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -14,28 +14,31 @@ class Solution {
         ListNode temp = head;
         
         int size =0;
-        
+        //find the size of the linkedlist
         while(temp != null){
             temp = temp.next;
             size++;
         }
-        
+        // if linked list size is one so return the empty linked list
         if(size == 1 && n==1){
             return head=null;
         }
         
         size = size -n;
+        // remove first node of the linkedlist
         if(size == 0){
             return head.next;
         }
         temp = head;
         int i =1;
+        
+        // going to the i th node of the linked list
         while (i< size){
             temp = temp.next;
             i++;
         }
-        System.out.print(temp.val);
-        
+       
+        // remove the i th node 
         temp.next = temp.next.next;
         
         return head;
