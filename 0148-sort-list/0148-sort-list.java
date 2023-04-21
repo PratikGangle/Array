735b6c6cf8089285d11745c7ec56b2c4900d4149
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-04-21 10:05:23 -0700

    Time: 17 ms (24.22%), Space: 54 MB (12.94%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,33 +11,34 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         
+        // if ListNode is empty return empty node
         if(head == null){
             return head;
         }
         ArrayList<Integer> list = new ArrayList<>();
         
+        // store ListNode value in the ArrayList
         ListNode temp = head;
         while(temp != null){
             list.add(temp.val);
             temp = temp.next;
         }
         
+        // Sort the ArrayList
         Collections.sort(list);
-        
-        System.out.print(list);
-        
-         ListNode ans = new ListNode(list.get(0));       
+      
+        ListNode ans = new ListNode(list.get(0));       
         
         ListNode tempNode = ans;
         
-        for(int i =1; i<list.size(); i++){
-            
+        // create a new listNode in which element is stored order
+        for(int i =1; i<list.size(); i++){            
             int val = list.get(i);
             ListNode node = new ListNode(val);
             tempNode.next = node;
             tempNode = tempNode.next;
         }
-        
+        // return the head of the answer Node
         return ans;
     }
 }
