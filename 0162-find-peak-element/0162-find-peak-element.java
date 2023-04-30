d4f5b663b22ea21c13ef9f9f0ad65d38ff13dbca
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-04-30 10:59:21 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (33.32%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,19 +1,21 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         
-        // if array contain one elelment return index 0
-        if(nums.length == 1){
-            return 0;
-        }
-       
-        // find the peek element
-        for(int i =0; i<nums.length-1; i++){            
-            if(nums[i] > nums[i+1]){
-                return i;
+      
+        int start =0;
+        int end =nums.length-1;
+        
+        
+        while (start < end){
+            int mid = start +(end- start)/2;
+            
+            if(nums[mid] > nums[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid +1;
             }
         }
-        // if we not found the i th element that meaning array list sorted
-        // so we are reture the last index of the array
-        return nums.length-1;
+        return start;
     }
 }
