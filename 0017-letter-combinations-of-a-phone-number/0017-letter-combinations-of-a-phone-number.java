c422a15b36a458ff755a5af670c1bd9ad4e8cf23
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-05-12 00:01:19 +0530

    Time: 5 ms (43.59%), Space: 42.5 MB (40.88%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,30 +2,32 @@ class Solution {
     
     public List<String> letterCombinations(String digits) {
         
-        Map<Integer , String > map = new HashMap<>();
-        
-        map.put(2,"abc");
-        map.put(3,"def");
-        map.put(4,"ghi");
-        map.put(5,"jkl");
-        map.put(6,"mno");
-        map.put(7,"pqrs");
-        map.put(8,"tuv");
-        map.put(9,"wxyz");
-                
+         
+        String[] arr = new String[10];
+        arr[0] = "";
+        arr[1] = "";
+        arr[2] = "abc";
+        arr[3] = "def";
+        arr[4] = "ghi";
+        arr[5] = "jkl";
+        arr[6] = "mno";
+        arr[7] = "pqrs";
+        arr[8] = "tuv";
+        arr[9] = "wxyz";
+                        
          List<String> list = new ArrayList<>();
         
         // if digits string  is empty return empty list
         if(digits.equals("")){
             return list;
         }
-         helper(digits, "", map , list);
+         helper(digits, "", arr , list);
 
         return list;
     }
     
     
-    public void helper(String in, String op, Map<Integer,String> map , List<String> list ) {
+   public void helper(String in, String op, String[] arr, List<String> list) {
 
         if (in.isEmpty()) {
             list.add(op);
@@ -33,11 +35,11 @@ class Solution {
         }
 
         int digit = in.charAt(0) - '0'; // this convert '2' to 2
-        String str = map.get(digit);  
+        String str = arr[digit];
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            helper(in.substring(1), op + ch , map , list);
+            helper(in.substring(1), op + ch, arr, list);
         }
 
     }
