fe81da79d839173f146f3961e15cd74f91ebe4d2
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2023-07-04 19:33:56 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (6.92%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,20 +1,19 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
-    
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i =0; i<nums.length; i++){
-            if(nums[i] != val){
-                list.add(nums[i]);
+        int i = 0;
+        int j =  nums.length -1;
+        while(i <= j){
+            if(nums[i] == val){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+            else{
+                i++;
             }
         }
-        int ans = list.size();
         
-        for(int i=0; i<list.size(); i++){
-            nums[i] = list.get(i);
-        }
-        
-        return ans;
+        return j+1;
     }
 }
