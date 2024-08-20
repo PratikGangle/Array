468f5fb3d771ddc6d71a4c51033a0d46e25272b2
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-08-20 17:04:39 +0530

    Time: 3 ms (88.16%), Space: 45.1 MB (36.53%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -1,10 +1,41 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
+//         Arrays.sort(nums);
+//         int n = nums.length;
         
-        int firstPair = nums[n-1] * nums[n-2];            
-        int secondPair =nums[0] * nums[1];
+//         int firstPair = nums[n-1] * nums[n-2];            
+//         int secondPair =nums[0] * nums[1];
+        
+        int a =Integer.MIN_VALUE;
+        int b =Integer.MIN_VALUE;          
+        int c = Integer.MAX_VALUE;            
+        int d = Integer.MAX_VALUE;
+        
+        for(int i=0; i<nums.length; i++){
+            // for max value
+            if(nums[i] > a){
+                b = a;
+                a = nums[i];
+            }
+            else{
+                if(nums[i] > b){
+                    b = nums[i];
+                }
+            }
+            
+            //for min value;            
+            if(nums[i] < c){               
+                d = c;               
+                c = nums[i];            
+            }
+            else{
+                if(nums[i] < d){
+                    d = nums[i];
+                }
+            }
+        }
+        int firstPair = a * b;            
+        int secondPair =c * d;
         
         return firstPair - secondPair;
     }
