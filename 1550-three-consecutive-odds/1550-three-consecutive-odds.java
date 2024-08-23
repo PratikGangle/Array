202090832debab4e27ae0cb468261a9e74e99569
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-08-23 18:18:34 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (7.62%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int count = 0;        
-        for(int i=0; i<arr.length-2; i++){
-            if(arr[i] %2 != 0 && arr[i+1] %2 != 0 && arr[i+2] %2 != 0){
-                return true;            
-            }                       
+        for(int i=0; i<arr.length; i++){          
+            if(arr[i] % 2 != 0) {
+               count++;
+               if(count == 3)
+                   return true;                         
+            }        
+            else
+                count = 0;                                  
         }
         return false;
     }
