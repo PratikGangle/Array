34089c01afe01317a812272f42c34911d5bc2f2f
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-08-25 20:17:30 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (96.59%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean divisorGame(int n) {        
-        if(n <= 1 ) return false;        
-        for(int i=1; i<n; i++){
-            if(n % i == 0){
-                return !divisorGame(n-i);
-            }
-        }
+        // if(n <= 1 ) return false;        
+        // for(int i=1; i<n; i++){
+        //     if(n % i == 0){
+        //         return !divisorGame(n-i);
+        //     }
+        // }
+        // return false;
+        
+        if(n%2 == 0) return true;
         return false;
     }
 }
