4225e88b47de179fbebb376ee0cc36b852e44d0f
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-09-02 16:50:04 +0530

    Time: 17 ms (86.02%), Space: 94.6 MB (30.38%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -4,23 +4,17 @@ class Solution {
         solve(1 , n , k ,new ArrayList<>(), ans);
         return ans;
     }
-    
-    
+        
      public void solve (int start ,int n, int k, List<Integer> list , List<List<Integer>> ans) {
          if(k == 0){
              ans.add(new ArrayList<>(list));
              return;
          }         
-         if(start > n){
-             return;
-         }
-         
-         // take
-         list.add(start);
-         solve(start+1, n, k-1, list,ans);
-         list.remove(list.size()-1);
-         
-         // not take
-         solve(start+1,n,k,list,ans);       
+      
+         for(int i= start ; i<=n; i++){
+             list.add(i);       
+             solve(1+i, n, k-1, list,ans);      
+             list.remove(list.size()-1);
+         }         
     }    
 }
