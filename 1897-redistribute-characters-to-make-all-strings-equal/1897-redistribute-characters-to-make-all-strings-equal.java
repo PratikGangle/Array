609ac126de0d3cc0490dcde1cc86fbdd0305d89e
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-10-06 23:57:18 +0530

    Time: 1 ms (100.00%), Space: 43.5 MB (67.08%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
@@ -1,17 +1,27 @@
 class Solution {
     public boolean makeEqual(String[] words) {         
-        int map[] = new int[26];
-        for (String word : words) {
-            for (char c : word.toCharArray()) {
-                map[c - 'a']++;
-            }
-        }
-        int numStrings = words.length;
-        for (int count : map) {
-            if (count % numStrings != 0) {
-                return false;
-            }
-        }
-        return true;
+        if (words.length == 1) {
+			return true;
+		}
+		int totalCharCount = 0;
+		for (String s : words) {
+			totalCharCount += s.length();
+		}
+		if (totalCharCount % words.length != 0) {
+			return false;
+		}
+
+		int[] myMap = new int[26];
+		for (String s : words) {
+			for (char c : s.toCharArray()) {
+				myMap[c - 'a']++;
+			}
+		}
+		for (int i : myMap) {
+			if (i % words.length != 0) {
+				return false;
+			}
+		}
+		return true; 
     }
 }
